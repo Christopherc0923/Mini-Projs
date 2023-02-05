87ffc051e4beebfb9680b207cdda0ba1214d3ed4
Christopherc0923 <42985146+Christopherc0923@users.noreply.github.com>
2023-02-04 23:46:32 -0500

    minimal style changes

--- Django2/personalWeb/stock/views.py
+++ Django2/personalWeb/stock/views.py
@@ -42,7 +42,7 @@ def update_stocks(time_period, stocks):
             print(f"Key error: {e}")
             print(data)
 
-update_stocks(30, stocks)
+update_stocks(10, stocks)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
