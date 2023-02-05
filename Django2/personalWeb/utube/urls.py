3712bb438a7b1868a250f6305f1f3818e131e3c8
Christopherc0923 <42985146+Christopherc0923@users.noreply.github.com>
2023-02-05 17:48:40 -0500

    update utube app

--- Django2/personalWeb/utube/urls.py
+++ Django2/personalWeb/utube/urls.py
@@ -6,4 +6,6 @@ app_name = "utube"
 
 urlpatterns = [
     path('', views.utube, name="utube"),
+    path('download_video/', views.download_video, name='download_video'),
+    path('show_video/<str:video_title>/', views.show_video, name='show_video'),
 ] 
